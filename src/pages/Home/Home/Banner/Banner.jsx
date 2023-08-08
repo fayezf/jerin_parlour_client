23e88883594a2746b56220b2f7df9eb44cb1d8b4
fayezf <mohammadfayezahmed835@gmail.com>
2023-08-08 14:00:24 +0600

    banner and nav customize

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div className="hero bg-orange-50 flex gap-8 items-center justify-center">
-            <div className="hero-content flex-col lg:flex-row-reverse space-x-12">
+        <div className="hero bg-orange-50 flex items-center justify-center">
+            <div className="hero-content flex-col md:flex-row-reverse space-x-12">
                 <img src={banner} className="max-w-sm rounded-lg shadow-2xl" />
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-semibold">BEAUTY SALON <br /> FOR EVERY WOMEN</h1>
